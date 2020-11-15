0e154ed423bf8cfe08c703639b91424a1ce2a395
VZykova <VZykova@croc.ru>
2020-11-15 20:56:02 +0500

    task 2

--- 02-javascript-data-types/2-pick/index.js
+++ 02-javascript-data-types/2-pick/index.js
@@ -5,5 +5,11 @@
  * @returns {object} - returns the new object
  */
 export const pick = (obj, ...fields) => {
-
+    let newObj = {};
+    for (let arg in fields) {
+        if (fields[arg] in obj) {
+            newObj[fields[arg]] = fields[arg];
+        }
+    };
+    return newObj;
 };
